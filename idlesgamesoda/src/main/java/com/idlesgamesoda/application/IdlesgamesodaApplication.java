74ff12e09447efe92a795d9f3f6840c2f17bd730
Alexandre Pignon <alexandre.pignon@cgi.com>
2024-12-03 15:01:12 +0100

    create java spring project
    create folders and files, add first parameters for Spring. Launch Ok

---
+++ idlesgamesoda/src/main/java/com/idlesgamesoda/application/IdlesgamesodaApplication.java
@@ -0,0 +1,13 @@
+package com.idlesgamesoda.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdlesgamesodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdlesgamesodaApplication.class, args);
+	}
+
+}
---
+++ idlesgamesoda/src/test/java/com/idlesgamesoda/idlesgamesoda/IdlesgamesodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idlesgamesoda.idlesgamesoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdlesgamesodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
